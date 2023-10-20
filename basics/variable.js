c691a6194e16a1e867f6a2e72fb4dbab87c603b9
Ayushi Awasthi <113617557+Ayushiawasthi26@users.noreply.github.com>
2023-10-20 17:52:47 +0000

    variable

---
+++ basics/variable.js
@@ -0,0 +1,12 @@
+const accountId=14453
+let email="ayushi@gmail.com"
+var accountPass="123345"
+acccity="Kanpur"
+
+
+// accountId=345 not allowed as const doesnot allow to change value
+
+ email="asdfg@gmail.com"
+ accountPass="98745"
+ acccity="Uhas"
+ console.table([accountId,email,accountPass,acccity]);
