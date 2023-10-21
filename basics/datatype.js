0806919d1196747c52006a94eb0ee7ba8b209804
Ayushi Awasthi <113617557+Ayushiawasthi26@users.noreply.github.com>
2023-10-21 10:07:02 +0000

    learnt more about datatype and type

---
+++ basics/datatype.js
@@ -0,0 +1,21 @@
+"use strict";// all newer js version
+
+//alert(3+3) only iused in browser not in  node js
+
+let name="Ayushi";
+let age=19;
+let isLogged=true;
+let state;
+
+
+//number=>2 to power 53
+//boolean=>true/false
+//string=""
+//bigint
+//null=standalone value
+//undefined=undefined
+//symbol=>unique
+
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
--- basics/variable.js
+++ basics/variable.js
@@ -2,11 +2,15 @@ const accountId=14453
 let email="ayushi@gmail.com"
 var accountPass="123345"
 acccity="Kanpur"
+let account;// it gives value as undefined if we don't initialise the value
 
+/*
+prefer not to use var 
+because of issue in block scope and functional scope*/
 
-// accountId=345 not allowed as const doesnot allow to change value
+// accountId=345 not allowed as const does not allow to change value
 
  email="asdfg@gmail.com"
  accountPass="98745"
  acccity="Uhas"
- console.table([accountId,email,accountPass,acccity]);
+ console.table([accountId,email,accountPass,acccity,account]);
