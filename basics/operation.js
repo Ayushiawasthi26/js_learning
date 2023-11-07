2d7f8f9e0aadfbdf221feee67fa011ff6e96ee77
Ayushi Awasthi <113617557+Ayushiawasthi26@users.noreply.github.com>
2023-11-07 19:24:05 +0000

    updated

---
+++ basics/operation.js
@@ -0,0 +1,16 @@
+// console.log("1"+2)12
+// console.log(2-2)  0
+// console.log(1+"2") 12
+// console.log(2+2)   4
+// console.log(2*2)   4
+// console.log(2%2)   0
+// console.log(2/2)   1
+
+
+// console.log(true); true
+// console.log(+true);  1
+
+// console.log(+" ");0
+let game=200;
+game++;
+console.log(game);
