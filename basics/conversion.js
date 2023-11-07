ab5377419c834a1db8d4b7b16ec71aaf7ffadaf5
Ayushi Awasthi <113617557+Ayushiawasthi26@users.noreply.github.com>
2023-11-07 19:10:56 +0000

    js resumed

---
+++ basics/conversion.js
@@ -0,0 +1,26 @@
+// let score=33;   //number
+let score="33abc";
+console.log(typeof score);
+console.log(typeof(score));  
+
+// score=0;  0
+// scroe=undefined; NaN
+// score=true; 1 ,false ; 0
+
+
+let valueInnumber=Number(score);
+// console.log(valueIn number);   NaN
+console.log(typeof valueInnumber);
+
+let isloggedIn=1;
+console.log(isloggedIn);
+let booleanisloggedIn = Boolean(isloggedIn);
+console.log(booleanisloggedIn);
+
+// islogged=" " false;
+// islogged="Ayushi"  true
+
+let some=33;
+let stringnumber=String(some);
+console.log(stringnumber);
+console.log(typeof(stringnumber));
